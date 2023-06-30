669f2f4af586cbb28c7645d483772e68fa714989
HimuHimu112002 <mdhmaktaruzzaman9101@gmail.com>
2023-06-30 10:02:59 +0600

    backend

---
+++ controllers/otpMatchController.js
@@ -0,0 +1,24 @@
+const User = require("../model/usersModel.js")
+
+let OtpMatchController = async (req, res)=>{
+    let {email, randomOtp} = req.body
+    let OtpMatchFind = await User.find({email})
+
+    if(OtpMatchFind.length > 0){
+        if(randomOtp == OtpMatchFind[0].randomOtp){
+            // otp remove section start
+            await User.findOneAndUpdate(
+                {email},
+                {$unset: {randomOtp:""}},
+                //{new: true}
+                        
+            )
+
+            res.json({success: "Otp matching"})
+        }else{
+            res.json({error: "Otp not matching"})
+
+        }
+    }
+}
+module.exports = OtpMatchController
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,9 +2,11 @@ const express = require('express')
 const router = express.Router()
 const registrationController = require("../../controllers/registrationController.js")
 const loginController = require("../../controllers/loginController.js")
+const OtpMatchController = require('../../controllers/otpMatchController.js')
 
 router.post("/registration",registrationController)
 router.post("/login", loginController)
+router.post("/otp", OtpMatchController)
 
 
 module.exports = router
